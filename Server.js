268887595faf8b32f42112a4411f43df3061f10a
Ashok Tulachan <ashok.tulachan@capitalone.com>
2017-03-13 15:27:17 -0400

    Updated to port

--- Server.js
+++ Server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 3800;
+
 app.get('/lists', function (req, res) {
    res.send({"data":[{"credit_card_payment":"YES","country":"US","wounded":"NO","cashtapp_enabled":"YES","address":"120 EASTSHORE DR, GLEN ALLEN, VA 23059-5644","twenty_four_hour_access":"NO","city":"GLEN ALLEN","state":"Virginia","atm_id":"ATM107","zip_code":"23059"},{"credit_card_payment":"YES","country":"US","wounded":"NO","cashtapp_enabled":"YES","address":"120 EASTSHORE DR, GLEN ALLEN, VA 23059-5644","twenty_four_hour_access":"NO","city":"GLEN ALLEN","state":"Virginia","atm_id":"ATM107","zip_code":"23059"},{"credit_card_payment":"YES","country":"US","wounded":"NO","cashtapp_enabled":"YES","address":"120 EASTSHORE DR, GLEN ALLEN, VA 23059-5644","twenty_four_hour_access":"NO","city":"GLEN ALLEN","state":"Virginia","atm_id":"ATM107","zip_code":"23059"},{"credit_card_payment":"YES","country":"US","wounded":"NO","cashtapp_enabled":"YES","address":"120 EASTSHORE DR, GLEN ALLEN, VA 23059-5644","twenty_four_hour_access":"NO","city":"GLEN ALLEN","state":"Virginia","atm_id":"ATM107","zip_code":"23059"},{"credit_card_payment":"YES","country":"US","wounded":"NO","cashtapp_enabled":"YES","address":"120 EASTSHORE DR, GLEN ALLEN, VA 23059-5644","twenty_four_hour_access":"NO","city":"GLEN ALLEN","state":"Virginia","atm_id":"ATM107","zip_code":"23059"},{"credit_card_payment":"YES","country":"US","wounded":"NO","cashtapp_enabled":"YES","address":"120 EASTSHORE DR, GLEN ALLEN, VA 23059-5644","twenty_four_hour_access":"NO","city":"GLEN ALLEN","state":"Virginia","atm_id":"ATM107","zip_code":"23059"},{"credit_card_payment":"YES","country":"US","wounded":"NO","cashtapp_enabled":"YES","address":"120 EASTSHORE DR, GLEN ALLEN, VA 23059-5644","twenty_four_hour_access":"NO","city":"GLEN ALLEN","state":"Virginia","atm_id":"ATM107","zip_code":"23059"}]} );
 })
 
-var server = app.listen(8084, function () {
+var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
 
